d96c0dd1dc646a6da1d9a3441bb74c0f465062f4
zlsheepcity <zl.sheepcity@gmail.com>
2023-09-02 16:40:41 +0300

    GEN4 test

---
+++ gen9/GEN4.js
@@ -0,0 +1,136 @@
+// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+const GeneticsLibrary = function (injectDNA){
+    let GEN = Object.assign(this, injectDNA);
+    // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ Seed
+
+    GEN.v = '23.9.2'
+    GEN.name = 'GEN4'
+    GEN.SEED = 'RGFH'
+    const UTT = {
+        RND: C=> C? Math.floor(Math.random()*C):Math.random(),
+    };
+
+    // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ Root
+
+    // (G) => 'GRFH'
+    GEN.Gene = base => {
+        const  r = GEN.SEED
+        const  g = r => f(h)+f(h+r)+f(h+r)+f(h+r)
+        const  f = x => x[UTT.RND(x.length)]
+        const  h = base || f(r)
+        return g(r);
+        };
+
+    // (3)(G) => ['GRFH','GHFR','GFHR']
+    GEN.Acid = level => base => {
+        const r = Array(level||0).fill(0)
+        const g = r => r.map(f)
+        const f = x => GEN.Gene(base)
+        return g(r);
+        };
+
+    // (0) => {R:[],G:[],F:[],H:[]}
+    GEN.Nuke = level => {
+        const  r = [...GEN.SEED]
+        const  g = x => x.reduce(f, {})
+        const  f = (a,G) => (a[G]=h(G),a)
+        const  h = G => GEN.Acid(level)(G)
+        return g(r);
+        };
+
+    // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ Gene
+
+    // (G) => 'GGGG'
+    GEN.GeneMask =
+        o => [...GEN.Gene()]
+               .fill(o||'O')
+                .join('');
+
+    // ('GRFH','GRFH') => 1
+    GEN.GeneLoveStimul =
+        (AAAA,BBBB) => [...AAAA].reduce(
+            (p,g,i) => BBBB[i] === g? ++p :p
+                ,0);
+
+    // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ Acid
+
+    // ? => some magic
+    GEN.AcidChallenge =
+       (a,b,c) => {
+        if (Array.isArray(a)) [a,b,c] = a
+        let h = GEN.GeneLoveStimul
+        let friendly = h(c,b) > 1
+        let winner =
+          1 < h(c,a) && UTT.RND()*h(c,a)
+            > UTT.RND()*h(b,a)
+            ? c:b;
+        return friendly ? [b,c] : [winner] ;
+        };
+
+    // ['RRRR','HHHH','RRFF'] => ['RRRR','RRFF']
+    GEN.AcidWeed = acid => acid.reduce(
+       (AcW,G) => {
+            const b = GEN.GeneMask(AcW[0] && AcW[0][0])
+            const h = AcW.length >1 ? AcW.slice(-2)[0]:b
+            const r = AcW.slice(0, -1)
+            const g = AcW.slice(-1)[0]
+            const f = GEN.AcidChallenge
+            return !!!AcW.length? [G] :[...r,...f(h,g,G)];
+        },[]);
+
+    // (['HHHH','HGGG'])('H') => 5
+    GEN.AcidStimul = acid => G =>
+        [...G].reduce(
+            (power,g) => power+
+            (acid.join('')
+                .match(new RegExp(g,'g')) || [])
+                    .length
+            ,0);
+
+    // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ Nuke
+
+    // (0) => {R:[],F:[],G:[],H:[]}
+    GEN.BabyNuke = level => {
+        const  r = GEN.Nuke(level)
+        const  g = G => GEN.AcidWeed(r[G])
+        const  f = (n,G) => ({...n, [G]:g(G)})
+        const  h = Object.keys(r).reduce(f,{})
+        return h; // {}
+        };
+
+    // ({H:['HHHH'],G:['GGGH']})('H') => 5
+    GEN.NukeStimul = nuke => G =>
+        Object.keys(nuke).reduce(
+            (power,g) => power+
+            GEN.AcidStimul(nuke[g])(G)
+            ,0);
+
+    // (oO{RGFH})({RGFH}) => oO{RGFH}
+    GEN.NukeFill = oO => fill => {
+        const acid = G => GEN.AcidWeed([...oO.nuke[G],...fill[G]]);
+        return Object.keys(fill).map( G=> {oO.nuke[G] =  acid(G)});
+        };
+
+    // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+};
+
+const GeneticsLibraryTest = f => {
+    const GEN = new GeneticsLibrary();
+    const m = GEN.Nuke(1)
+    const n = GEN.Nuke(3)
+    const o = {nuke:{...m}}
+    const d = GEN.NukeFill(o)(n)
+    const b = o.nuke
+    const t = 'GeneticsLibraryTest()'
+  f?console.groupCollapsed(t)
+   :console.group(t)
+    console.log('Gene:', GEN.Gene()         );
+    console.log('Acid:', GEN.Acid(3)('R')   );
+    console.log('Nuke:'); for (G in m)
+    console.log(G, GEN.NukeStimul(m)(G), m[G]);
+    console.log('Fill:'); for (G in n)
+    console.log(G, GEN.NukeStimul(n)(G), n[G]);
+    console.log('Nuke filled:'); for (G in b)
+    console.log(G, GEN.NukeStimul(b)(G), b[G]);
+    console.groupEnd();
+};
--- gen9/GameMaster.js
+++ gen9/GameMaster.js
@@ -1,8 +1,58 @@
-// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ import
-// const GMO = function factory (){}
+// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+const GMO = function GeneticallyModifiedObject
+(dna) {
+    const NameWizard = base => {
+        const Header = 'DMNLTGH'
+        const Bodies = ['ina','ora','ana','iko','uni']
+        const f = oo => oo[Math.floor( Math.random()*oo.length )]
+        return (base || f(Header)) + f(Bodies);
+    };
+    let GMO = Object.assign(this,dna)
+    let NukeStimul = Gen.NukeStimul
+    let NukeFill   = Gen.NukeFill
+    GMO.nuke       = Gen.BabyNuke(GMO.level||0)
+    GMO.name       = GMO.name || NameWizard()
+
+    GMO.Stimul = function (G) { return NukeStimul(this.nuke)(G) }
+
+    GMO.Run  = function (oO) { this.Fill(oO,['R']) }
+    GMO.Get  = function (oO) { this.Fill(oO,['G']) }
+    GMO.Fix  = function (oO) { this.Fill(oO,['F']) }
+    GMO.Hit  = function (oO) { this.Fill(oO,['H']) }
+    GMO.Fill = function (oO, keys) {
+        let N = {...this.nuke}
+        let K = keys || Object.keys(N)
+        K.map( G=> NukeFill(oO)({[G]:N[G]}) );
+        };
+
+    GMO.Profile = function (s) {
+        console[s?'groupCollapsed':'group']('oO:'+this.name)
+        for (G in this.nuke)
+        console.log(G, NukeStimul(this.nuke)(G), this.nuke[G])
+        console.groupEnd()
+        return 'Looks good' ;};
+};
+
+// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+const GameBall = function (injectDNA) {
+    const Ball = new GMO(injectDNA);
+    Object.assign(this, Ball);
+};
+const GameSheep = function (injectDNA) {
+    const Sheep = new GMO(injectDNA);
+    Object.assign(this, Ball);
+};
+
 // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 const GameMaster = function (injectDNA) {
     const masterDNA = {
+        props: {
+            v: '23.9.2',
+            scoreToWin: 21,
+            teamsSize: 3,
+            ballsLevel: 1,
+            squadFirstServe: 0,
+        },
         state: {
             point: 0,
             touch: 0,
@@ -14,60 +64,121 @@ const GameMaster = function (injectDNA) {
             balls: [],
             },
         calls: {
-            gameStarted:  report => false,
-            serviceReady: report => false,
-            serviceAfter: report => false,
+            onGameStarted: report => false,
+            onGameResults: report => false,
+            onSquadChange: report => false,
+            onTouchBefore: report => false,
+            onTouchAfter:  report => false,
         },
         rules: {
-            checkScoreToWin:
-                (C=>S=>S[0]>=C||S[1]>=C)(21)
-                ,// [0,0] => false
-            checkTeamsReady:
-                (F=>C=>([A,B])=>F(A)>=C&&F(B)>=C)(F=>F.length)(3)
-                ,// [[],[]] => false
-            passStageRun:
-                B=>((O=>O.H>O.G)({H:B.Stimul('H'),G:B.Stimul('G')}))
-                ,// GMO => false
-            },
+            checkScoreToWin:Game => {
+                const {score, scoreToWin} = Game;
+                const F = C=> G=> G[0]>=C||G[1]>=C;
+                return F(scoreToWin)(score);
+                },
+            checkTeamsReady:Game => {
+                const {teams, teamsSize} = Game;
+                const G = G=> G.length;
+                const F = C=> ([A,B])=> G(A)===C&&C===G(B);
+                return F(teamsSize)(teams);
+                },
+            passStageRun:Game => {
+                const {balls, point} = Game;
+                const ball = balls[point];
+                const ballStimuls = {
+                    R:ball.Stimul('R'),
+                    G:ball.Stimul('G'),
+                    F:ball.Stimul('F'),
+                    H:ball.Stimul('H'),
+                    };
+                const F = ([H,G])=>H>G;
+                return F(ballStimuls);
+                },
+            passStageGet:Game => {
+                const {balls, point} = Game;
+                const ball = balls[point];
+                const ballStimuls = {
+                    R:ball.Stimul('R'),
+                    G:ball.Stimul('G'),
+                    F:ball.Stimul('F'),
+                    H:ball.Stimul('H'),
+                    };
+                const F = ([H,G])=>G>H;
+                return F(ballStimuls);
+                },
+            passStageFix:Game => {
+                const {balls, point} = Game;
+                const ball = balls[point];
+                const ballStimuls = {
+                    R:ball.Stimul('R'),
+                    G:ball.Stimul('G'),
+                    F:ball.Stimul('F'),
+                    H:ball.Stimul('H'),
+                    };
+                const F = always => true;
+                return F(ballStimuls);
+                },
+            passStageHit:Game => {
+                const {balls, point} = Game;
+                const ball = balls[point];
+                const ballStimuls = {
+                    R:ball.Stimul('R'),
+                    G:ball.Stimul('G'),
+                    F:ball.Stimul('F'),
+                    H:ball.Stimul('H'),
+                    };
+                const F = ([H,G])=>H>G;
+                return F(ballStimuls);
+                },
+        },
     };
 
     // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
     const DNA = Object.assign(this,masterDNA,injectDNA);
+    const RNA = DNA.state;
     // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
-    const CALL = n => DNA.calls[n](DNA.report());
-    DNA.stateUpdate = state => Object.assign(DNA.state,state);
-    DNA.teamsRegister = ({teams}) => DNA.stateUpdate({teams});
+    DNA.CALL = N => DNA.calls[N](DNA.report());
+    DNA.RULE = N => DNA.rules[N](DNA.report());
+    DNA.SAVE = D => Object.assign(DNA.state,D);
 
     // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ .report
     DNA.report = () => ({
-        complete:
-            DNA.rules.checkScoreToWin(DNA.state.score),
-        prepared:
-            DNA.rules.checkTeamsReady(DNA.state.teams),
-        launched:
-            !!(DNA.state.point||DNA.state.touch),
-     ...DNA.state });
-
+        complete: DNA.RULE('checkScoreToWin'),
+        prepared: DNA.RULE('checkTeamsReady'),
+        launched: !!(DNA.state.point||DNA.state.touch),
+     ...DNA.state,
+     ...DNA.props,
+    });
     // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ .create
-    DNA.create = game => {
-        const {teams} = game;
-        teamsRegister({teams});
+    DNA.create =(O={})=> {
+        const {teams} = O;
+        DNA.SAVE({teams});
     };
-
     // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ .launch
     DNA.launch = () => {
-        DNA.calls.gameStarted(DNA.report());
-        DNA.stagePlay();
+        DNA.CALL('onGameStarted');
+        do { DNA.stagePlay() }
+        while (!DNA.report().complete);
+        DNA.finish();
     };
-
+    // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ .launch
+    DNA.finish = () => {
+        DNA.CALL('onGameResults');
+    };
     // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ BALLS
-    DNA.ballsTakeNow = (S=>F=>S.balls[S.point])(DNA.state);
-    DNA.ballsTakeNew = (bid=DNA.state.point) => {
-        const newball = new GMO({level:1,name:`Ball${bid}`});
-        DNA.state.balls[bid] = newball;
-        return DNA.ballsTakeNow();
-    };
+    DNA.ballsTakeNow = (R=>F=>R.balls[R.point])(RNA);
+    DNA.ballsTakeNew = (B=RNA.point) => {
+        RNA.balls[B] = DNA.ballsMakeNew(B);
+        return RNA.balls[B];
+        };
+    DNA.ballsMakeNew = id => new GMO({
+        level: DNA.props.ballsLevel,
+        name: `Ball${id}`,
+        });
+    DNA.ballsIsAlive = f => !!DNA.ballsTakeNow().inPlay ;
+    DNA.ballsSetPlay = f => { DNA.ballsTakeNow().inPlay = true };
+    DNA.ballsSetMiss = f => { DNA.ballsTakeNow().inPlay = false};
     // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ SHEEP
     DNA.sheepTakeNow = f => {
         const {teams,squad,sheep}=DNA.state;
@@ -79,131 +190,101 @@ const GameMaster = function (injectDNA) {
         };
     // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ SQUAD
     DNA.squadSetNext = f => { DNA.state.squad = DNA.state.squad ? 0:1 };
+    // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ SCORE
+    DNA.scorePushOne = f => { RNA.point++;RNA.score[RNA.squad]++ };
     // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ TOUCH
-    DNA.touchSetBase = f => { DNA.state.touch=0 };
-    DNA.touchSetNext = f => { DNA.state.touch++ };
+    DNA.touchSetBase = f => { RNA.touch=0 };
+    DNA.touchSetNext = f => { RNA.touch++ };
+    DNA.touchContact = f => {
+        const Ball  = DNA.ballsTakeNow();
+        const Sheep = DNA.sheepTakeNow();
+        const touch = ['Run','Get','Fix','Hit'][RNA.stage];
+        const pass  = name => DNA.RULE(`passStage${name}`);
+     // make contact
+        DNA.touchSetNext();
+        Sheep[touch](Ball);
+        return pass(touch);
+    };
     // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ STAGE
     DNA.stageSetBase = f => { DNA.stagePush(false) };
     DNA.stageSetNext = f => { DNA.stagePush(true)  };
     DNA.stagePush = keepPlaying => {
         keepPlaying
-          ? DNA.state.stage ++
-          : DNA.state.stage =0
-        if( DNA.state.stage >3 )
-            DNA.state.stage =1 };
-    DNA.stagePlay = f => [
-        RUN => DNA.stagePlayRUN(),
-        GET => DNA.stagePlayGET(),
-        FIX => DNA.stagePlayFIX(),
-        HIT => DNA.stagePlayHIT(),
-        ][DNA.state.stage]();
+          ? RNA.stage ++
+          : RNA.stage =0 ;
+        if( RNA.stage >3 )
+            RNA.stage =1 };
+    DNA.stagePlay = f => {
+        const run = [
+            RUN => DNA.stagePlayRUN(),
+            GET => DNA.stagePlayGET(),
+            FIX => DNA.stagePlayFIX(),
+            HIT => DNA.stagePlayHIT(),
+            ][RNA.stage] ;
+        const fix = f => {
+            let inPlay =DNA.ballsIsAlive();
+            if(!inPlay) DNA.scorePushOne();
+            if(!inPlay) DNA.stageSetBase();
+            };
+        run();
+        fix();
+    };
     DNA.stagePlayRUN = f => {
-
-        // Take
+     // Ready
+        DNA.stageSetBase();
         DNA.touchSetBase();
-        const Ball  = DNA.ballsTakeNew();
-        const Sheep = DNA.sheepTakeNow();
-        Ball.inPlay = true;
-        CALL('serviceReady');
-
-        // Fill
-        Sheep.Hit(Ball);
-        DNA.touchNext();
-
-        // Ball
-        let pass = DNA.passStageRun(Ball);
-        let miss = !pass
-        let none = false
-        if (miss) DNA.sheepSetNext()
-        if (true) DNA.squadSetNext()
-        if (pass) DNA.stageSetNext()
-        if (miss) Ball.inPlay = false
-        CALL('serviceAfter');
+        DNA.ballsTakeNew();
+        DNA.ballsSetPlay();
+        DNA.CALL('onSquadChange');
+        DNA.CALL('onTouchBefore');
+     // Touch
+        let pass = DNA.touchContact();
+        let miss = !pass;
+        let none = false;
+        if (miss) DNA.sheepSetNext();
+        if (true) DNA.squadSetNext();
+        if (pass) DNA.stageSetNext();
+        if (miss) DNA.ballsSetMiss();
+        DNA.CALL('onTouchResult');
     };
-
-};
-const GameMaster = function (injectDNA) {
-    const MasterState = function () {
-        let GMS = this;
-        GMS.getStateInitial = () => ({
-            point: 0,
-            touch: 0,
-            stage: 0,
-            squad: 0,
-            sheep: [0,0],
-            score: [0,0],
-            teams: [[],[]],
-            balls: [],
-        });
-        GMS.data = GMS.getStateInitial();
-        GMS.values = v => GMS.data;
-        GMS.update = v => Object.assign(GMA.data,v);
+    DNA.stagePlayGET = f => {
+     // Ready
+        DNA.CALL('onSquadChange');
+        DNA.CALL('onTouchBefore');
+     // Touch
+        let pass = DNA.touchContact();
+        let miss = !pass;
+        let none = false;
+        if (true) DNA.sheepSetNext();
+        if (miss) DNA.squadSetNext();
+        if (pass) DNA.stageSetNext();
+        if (miss) DNA.ballsSetMiss();
+        DNA.CALL('onTouchResult');
     };
-    const GMA = Object.assign(this);
-    GMA.rules = {
-        stageOrder: ['RUN','GET','FIX','HIT'],
-        checkScoreToWin:
-            // [0,0] => false
-            (C=>S=>S[0]>=C||S[1]>=C)(21),
-        };
-    GMA.stateInitial = () => ({
-        point: 0,
-        touch: 0,
-        stage: 0,
-        squad: 0,
-        sheep: [0,0],
-        score: [0,0],
-        teams: [[],[]],
-        balls: [],
-        });
-    const RNA = GMA.stateInitial();
-
-    GMA.state = RNA;
-    GMA.stateUpdate = state => Object.assign(GMA.state,state);
-    GMA.stateReport = () => ({
-        complete: GMA.rules.checkScoreToWin(RNA.score),
-        launched: !!(RNA.point||RNA.touch),
-        ...RNA});
-
-    GMA.registration = data => {
-        const {teams} = data;
-        GMA.stateUpdate({teams});
+    DNA.stagePlayFIX = f => {
+     // Ready
+        DNA.CALL('onTouchBefore');
+     // Touch
+        let pass = DNA.touchContact();
+        let miss = !pass;
+        let none = false;
+        if (pass) DNA.sheepSetNext();
+        if (miss) DNA.squadSetNext();
+        if (pass) DNA.stageSetNext();
+        if (miss) DNA.ballsSetMiss();
+        DNA.CALL('onTouchResult');
     };
-
-    GMA.stageClear = rna => {
-        GMA.stateUpdate({
-            
-        });
+    DNA.stagePlayHIT = f => {
+     // Ready
+        DNA.CALL('onTouchBefore');
+     // Touch
+        let pass = DNA.touchContact();
+        let miss = !pass;
+        let none = false;
+        if (none) DNA.sheepSetNext();
+        if (true) DNA.squadSetNext();
+        if (pass) DNA.stageSetNext();
+        if (miss) DNA.ballsSetMiss();
+        DNA.CALL('onTouchResult');
     };
-
-    const DNA = {};
-    DNA.state = {
-        point: 0,
-        touch: 0,
-        stage: 0,
-        squad: 0,
-        sheep: [0,0],
-        score: [0,0],
-        teams: [[],[]],
-        balls: [],
-        };
-    DNA.rules = {
-        stageOrder = ['RUN','GET','FIX','HIT'];
-        scoreToWin = 21;
-        };
-    const RNA = Object.assign(this,injectDNA);
-    RNA.state = { ...DNA.state };
-    RNA.rules = { ...DNA.rules };
-
-
-    // workers
-    RNA.stateUpdate = state => Object.assign(RNA.state,state);
-
-
-    RNA.preparations = rna => {
-        
-    };
-    RNA.report = topic => {
-      //DNA.report('registration');
-    };
-};
+};  // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ /GameMaster
